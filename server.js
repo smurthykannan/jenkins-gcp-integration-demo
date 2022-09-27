fd0d7047c57930d2bcb679a1dd941e12ec546039
kannan <kannan@gmail.com>
2022-09-27 12:37:07 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = process.env.PORT || 3000;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.get('/kannan', function (req, res) {
+    res.send('Hello kannan\n');
+});
+
+app.get('/upahar', function (req, res) {
+res.send('Hello upahar\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
